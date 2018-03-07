b48aa23d3a13c8f7f6939288b4ea2a435ecae0ed
HEADS <jinn0702@126.com>
2018-03-07 13:37:58 +0800

    增加bbr脚本
    增加bbr脚本

---
+++ run.sh
@@ -0,0 +1,7 @@
+sudo -i
+wget -N --no-check-certificate https://raw.githubusercontent.com/FunctionClub/YankeeBBR/master/bbr.sh && bash bbr.sh install
+y
+sudo -i 
+bash bbr.sh start
+wget --no-check-certificate https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocksR.sh&& chmod +x shadowsocksR.sh 
+./shadowsocksR.sh
