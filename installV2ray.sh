450db253fbad22bd8e53fdeb28e79fd77002a60d
HEADS <jinn0702@126.com>
2018-05-06 09:44:10 +0800

    Update installV2ray.sh

--- installV2ray.sh
+++ installV2ray.sh
@@ -1,4 +1,4 @@
-wget--no-check-certificate https://github.com/ligl0702/ligl0702.github.io/blob/master/v2ray.tar.gz
+wget--no-check-certificate https://raw.githubusercontent.com/ligl0702/ligl0702.github.io/master/v2ray.tar.gz
 tar -zxvf  v2ray.tar.gz
 chmod +x v2ray.tar.gz/install.sh
 sh v2ray/install.sh
